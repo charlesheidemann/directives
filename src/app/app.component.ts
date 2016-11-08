66cda646373adf82b5dc35a2996c1475e041a97f
Charles Heidemann <c.heidemann@2g.tj.rs>
2016-11-08 19:18:20 -0200

    UnlessDir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HighlightDirective } from './highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @Component({
   selector: 'dir-root',
@@ -7,5 +8,15 @@ import { HighlightDirective } from './highlight.directive';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  private switch = true;
+
+  private items = [1, 2, 3, 4, 5];
+
+  private number = 100;
+
+  onSwitch() {
+    this.switch = !this.switch;
+  }
 
 }
--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,14 +1,37 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[dirHighlight]'
 })
 export class HighlightDirective {
 
-  private elementRef: ElementRef;
+  private backgroundColour = this.defaultColor;
 
-  constructor(elementRef: ElementRef) {
-    this.elementRef = elementRef;
+  @Input() defaultColor = 'white';
+  @Input('highlight') highlightColor = 'green';
+
+
+  @HostListener('mouseenter') over() {
+    this.backgroundColour = this.highlightColor;
+  };
+
+  @HostListener('mouseleave') leave() {
+    this.backgroundColour = this.defaultColor;
+  };
+
+  @HostBinding('style.backgroundColor') get setColor() {
+    return this.backgroundColour;
+  };
+
+  ngOnInit() {
+    this.backgroundColour = this.defaultColor;
+  }
+
+  constructor() {
+    //private elementRef: ElementRef, private renderer: Renderer
+    //this.elementRef.nativeElement.style.backgroundColor = 'green';
+    //this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
+
   }
 
 }
